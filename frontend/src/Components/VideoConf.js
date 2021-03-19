1c49037d9e6bc435132d05de23ce2a9876386554
Tejas Ghone <tejasghone73@gmail.com>
2021-03-19 22:20:41 +0530

    initial commit

---
+++ frontend/src/Components/VideoConf.js
@@ -0,0 +1,9 @@
+
+import React from 'react';
+
+
+const VideoConf=()=> {
+    const greeting = 'Jalebi FAFDA!';
+    return <h1>{greeting}</h1>;
+  }
+export default VideoConf;
---
+++ frontend/src/Components/WhiteBoard.js
@@ -0,0 +1,6 @@
+import React from 'react';
+const WhiteBoard=()=> {
+    const greeting = 'Jalebi FAFDA!';
+    return <h1>{greeting}</h1>;
+  }
+export default WhiteBoard;
