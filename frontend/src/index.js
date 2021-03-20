e7ee14c2433796da8c7190d4512379dec1c2b0d3
YatharthVyas <yatharth2000.vyas@gmail.com>
2021-03-20 13:48:46 +0530

    Update backend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,9 +4,9 @@ import "./index.css";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import App from "./App";
 import { CustomThemeProvider } from "./themes/CustomThemeProvider";
-//import axios from "axios";
+// import axios from "axios";
 
-//axios.defaults.baseURL = "https://virtualclassloc.herokuapp.com/"; //"http://localhost:8000";
+// axios.defaults.baseURL = "https://virtualclassloc.herokuapp.com/"; //"http://localhost:8000";
 ReactDOM.render(
 	<React.Fragment>
 		<CustomThemeProvider>
