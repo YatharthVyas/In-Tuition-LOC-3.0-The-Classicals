b0ff96befb955cbba912d846cec3a1eca9af1ef1
VatsalSoni_13 <sonivatsal2001@gmail.com>
2021-03-20 19:12:44 +0530

    Chat connection to firebase

--- frontend/src/Components/WhiteBoard.js
+++ frontend/src/Components/WhiteBoard.js
@@ -12,211 +12,211 @@ import firebase from "./firebase";
 import "firebase/database";
 
 const useStyles = makeStyles((theme) => ({
-	root: {
-		marginTop: -20,
-		marginLeft: "-10%",
-	},
-	canvas: {
-		backgroundColor: "#ededed",
-		boxShadow:
-			" 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
-	},
-	deleteIconBtn: {
-		backgroundColor: "#cc0000",
-		color: "white",
-		"&:hover": {
-			backgroundColor: "#ff0000",
-		},
-		marginLeft: 5,
-		marginBottom: 10,
-	},
-	drawBlue: {
-		backgroundColor: "#1111ff !important",
-		color: "white",
-		marginLeft: 5,
-		marginBottom: 10,
-	},
-	drawOrange: {
-		backgroundColor: "orange !important",
-		color: "white",
-		marginLeft: 5,
-		marginBottom: 10,
-	},
-	drawBlack: {
-		backgroundColor: "black !important",
-		color: "white",
-		marginLeft: 5,
-		marginBottom: 10,
-	},
+  root: {
+    marginTop: -20,
+    marginLeft: "-10%",
+  },
+  canvas: {
+    backgroundColor: "#ededed",
+    boxShadow:
+      " 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+  },
+  deleteIconBtn: {
+    backgroundColor: "#cc0000",
+    color: "white",
+    "&:hover": {
+      backgroundColor: "#ff0000",
+    },
+    marginLeft: 5,
+    marginBottom: 10,
+  },
+  drawBlue: {
+    backgroundColor: "#1111ff !important",
+    color: "white",
+    marginLeft: 5,
+    marginBottom: 10,
+  },
+  drawOrange: {
+    backgroundColor: "orange !important",
+    color: "white",
+    marginLeft: 5,
+    marginBottom: 10,
+  },
+  drawBlack: {
+    backgroundColor: "black !important",
+    color: "white",
+    marginLeft: 5,
+    marginBottom: 10,
+  },
 }));
 
 function Whiteboard(props) {
-	const width = 800;
-	const height = 500;
-	const classes = useStyles();
-	const [painting, setPainting] = useState(false);
-	const [updateDrawing, setUpdateDrawing] = useState(true);
-	const [erase, setErase] = useState(false);
-	const [ctx, setCtx] = useState(null);
-	const [lastX, setLastX] = useState();
-	const [lastY, setLastY] = useState();
-	const [img, setImg] = useState("");
-	const [imgOut, setImgOut] = useState("");
-	const canvas = useRef();
+  const width = 800;
+  const height = 500;
+  const classes = useStyles();
+  const [painting, setPainting] = useState(false);
+  const [updateDrawing, setUpdateDrawing] = useState(true);
+  const [erase, setErase] = useState(false);
+  const [ctx, setCtx] = useState(null);
+  const [lastX, setLastX] = useState();
+  const [lastY, setLastY] = useState();
+  const [img, setImg] = useState("");
+  const [imgOut, setImgOut] = useState("");
+  const canvas = useRef();
 
-	const drawCanvas = (e) => {
-		if (painting) {
-			let mousepos = findMousePos(e);
-			if (erase) {
-				ctx.clearRect(mousepos.x - 10, mousepos.y - 10, 20, 20);
-			} else {
-				ctx.beginPath();
-				ctx.moveTo(lastX, lastY);
-				ctx.lineTo(mousepos.x, mousepos.y);
-				ctx.closePath();
-				ctx.stroke();
-				setLastX(mousepos.x);
-				setLastY(mousepos.y);
-			}
-			if (updateDrawing) {
-				sendBoardToServer();
-				setUpdateDrawing(false);
-				setTimeout(() => setUpdateDrawing(true), 300);
-			}
-		}
-	};
+  const drawCanvas = (e) => {
+    if (painting) {
+      let mousepos = findMousePos(e);
+      if (erase) {
+        ctx.clearRect(mousepos.x - 10, mousepos.y - 10, 20, 20);
+      } else {
+        ctx.beginPath();
+        ctx.moveTo(lastX, lastY);
+        ctx.lineTo(mousepos.x, mousepos.y);
+        ctx.closePath();
+        ctx.stroke();
+        setLastX(mousepos.x);
+        setLastY(mousepos.y);
+      }
+      if (updateDrawing) {
+        sendBoardToServer();
+        setUpdateDrawing(false);
+        setTimeout(() => setUpdateDrawing(true), 300);
+      }
+    }
+  };
 
-	const onMouseDown = (e) => {
-		var m = findMousePos(e);
-		setLastX(m.x);
-		setLastY(m.y);
-		setPainting(true);
-	};
+  const onMouseDown = (e) => {
+    var m = findMousePos(e);
+    setLastX(m.x);
+    setLastY(m.y);
+    setPainting(true);
+  };
 
-	const onMouseUp = () => {
-		setPainting(false);
-		sendBoardToServer();
-	};
+  const onMouseUp = () => {
+    setPainting(false);
+    sendBoardToServer();
+  };
 
-	const findMousePos = (evt) => {
-		var rectBorder = canvas.current.getBoundingClientRect(); //Accounts for scroll distance and margin outside canvas
-		var mouseX = evt.clientX - rectBorder.left;
-		var mouseY = evt.clientY - rectBorder.top;
-		return {
-			x: mouseX,
-			y: mouseY,
-		};
-	};
+  const findMousePos = (evt) => {
+    var rectBorder = canvas.current.getBoundingClientRect(); //Accounts for scroll distance and margin outside canvas
+    var mouseX = evt.clientX - rectBorder.left;
+    var mouseY = evt.clientY - rectBorder.top;
+    return {
+      x: mouseX,
+      y: mouseY,
+    };
+  };
 
-	const clearCanvas = () => {
-		ctx.clearRect(0, 0, width, height);
-		ctx.fillStyle = "#ededed";
-		ctx.fillRect(0, 0, width, height);
-		sendBoardToServer();
-	};
+  const clearCanvas = () => {
+    ctx.clearRect(0, 0, width, height);
+    ctx.fillStyle = "#ededed";
+    ctx.fillRect(0, 0, width, height);
+    sendBoardToServer();
+  };
 
-	const sendBoardToServer = () => {
-		var imgdata = canvas.current.toDataURL();
-		setImg(imgdata);
-		firebase.database().ref(`Chats/abc`).update({ imgURL: imgdata });
-	};
+  const sendBoardToServer = () => {
+    var imgdata = canvas.current.toDataURL();
+    setImg(imgdata);
+    firebase.database().ref(`Chats/abc`).update({ imgURL: imgdata });
+  };
 
-	const setDrawColor = (color) => {
-		setErase(false);
-		ctx.strokeStyle = color;
-	};
+  const setDrawColor = (color) => {
+    setErase(false);
+    ctx.strokeStyle = color;
+  };
 
-	useEffect(() => {
-		setCtx(canvas.current.getContext("2d"));
-		var t = canvas.current.getContext("2d");
-		t.fillStyle = "#ededed";
-		t.fillRect(0, 0, width, height);
-		t.fillStyle = "black";
-		const onChildAdded = firebase
-			.database()
-			.ref(`Chats/abc`)
-			.on("child_changed", (snapshot) => {
-				setImgOut(snapshot.toJSON());
-				//console.log("VAL", snapshot.val());
-			});
-		return () =>
-			firebase.database().ref("Chats").off("child_added", onChildAdded);
-	}, []);
-	return (
-		<div className={classes.root}>
-			<Grid container>
-				<Grid item xs={9}>
-					<Grid container>
-						<Grid item xs={1}>
-							<Fab className={classes.deleteIconBtn} onClick={clearCanvas}>
-								<DeleteForeverIcon />
-							</Fab>
-							<br />
-							<Fab
-								className={classes.drawBlack}
-								onClick={() => setDrawColor("black")}
-							>
-								<CreateIcon />
-							</Fab>
-							<br />
-							<Fab
-								className={classes.drawBlue}
-								onClick={() => setDrawColor("blue")}
-							>
-								<CreateIcon />
-							</Fab>
-							<br />
-							<Fab
-								className={classes.drawOrange}
-								onClick={() => setDrawColor("orange")}
-							>
-								<CreateIcon />
-							</Fab>
-							<br />
-							<Fab
-								className={classes.deleteIconBtn}
-								onClick={() => setErase(true)}
-							>
-								<LayersClearIcon />
-							</Fab>
-							<br />
-						</Grid>
-						<Grid item xs={11}>
-							<canvas
-								ref={canvas}
-								id="myCanvas"
-								height={height}
-								width={width}
-								onMouseMove={drawCanvas}
-								onClick={drawCanvas}
-								onMouseDown={onMouseDown}
-								onMouseUp={onMouseUp}
-								className={classes.canvas}
-							></canvas>
-						</Grid>
-					</Grid>
-				</Grid>
-				<Grid item xs={3}>
-					<Chatbox />
-				</Grid>
-			</Grid>
-			{imgOut && (
-				<div>
-					<img src={imgOut} height="500px" width="800px" alt="whiteboard" />{" "}
-					<br />
-					<div align="center">
-						<a href={imgOut} download="Lecture_Screenshot.png">
-							<Button color="primary" variant="contained">
-								{" "}
-								<SystemUpdateAltIcon style={{ marginRight: 20 }} /> Capture
-								Notes
-							</Button>
-						</a>
-					</div>
-				</div>
-			)}
-		</div>
-	);
+  useEffect(() => {
+    setCtx(canvas.current.getContext("2d"));
+    var t = canvas.current.getContext("2d");
+    t.fillStyle = "#ededed";
+    t.fillRect(0, 0, width, height);
+    t.fillStyle = "black";
+    const onChildAdded = firebase
+      .database()
+      .ref(`WhiteBoard/abc`)
+      .on("child_changed", (snapshot) => {
+        setImgOut(snapshot.toJSON());
+        //console.log("VAL", snapshot.val());
+      });
+    return () =>
+      firebase.database().ref("WhiteBoard").off("child_added", onChildAdded);
+  }, []);
+  return (
+    <div className={classes.root}>
+      <Grid container>
+        <Grid item xs={9}>
+          <Grid container>
+            <Grid item xs={1}>
+              <Fab className={classes.deleteIconBtn} onClick={clearCanvas}>
+                <DeleteForeverIcon />
+              </Fab>
+              <br />
+              <Fab
+                className={classes.drawBlack}
+                onClick={() => setDrawColor("black")}
+              >
+                <CreateIcon />
+              </Fab>
+              <br />
+              <Fab
+                className={classes.drawBlue}
+                onClick={() => setDrawColor("blue")}
+              >
+                <CreateIcon />
+              </Fab>
+              <br />
+              <Fab
+                className={classes.drawOrange}
+                onClick={() => setDrawColor("orange")}
+              >
+                <CreateIcon />
+              </Fab>
+              <br />
+              <Fab
+                className={classes.deleteIconBtn}
+                onClick={() => setErase(true)}
+              >
+                <LayersClearIcon />
+              </Fab>
+              <br />
+            </Grid>
+            <Grid item xs={11}>
+              <canvas
+                ref={canvas}
+                id="myCanvas"
+                height={height}
+                width={width}
+                onMouseMove={drawCanvas}
+                onClick={drawCanvas}
+                onMouseDown={onMouseDown}
+                onMouseUp={onMouseUp}
+                className={classes.canvas}
+              ></canvas>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={3}>
+          <Chatbox />
+        </Grid>
+      </Grid>
+      {imgOut && (
+        <div>
+          <img src={imgOut} height="500px" width="800px" alt="whiteboard" />{" "}
+          <br />
+          <div align="center">
+            <a href={imgOut} download="Lecture_Screenshot.png">
+              <Button color="primary" variant="contained">
+                {" "}
+                <SystemUpdateAltIcon style={{ marginRight: 20 }} /> Capture
+                Notes
+              </Button>
+            </a>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Whiteboard;
--- frontend/src/Components/chatbox.js
+++ frontend/src/Components/chatbox.js
@@ -7,126 +7,150 @@ import Snackbar from "@material-ui/core/Snackbar";
 import Alert from "@material-ui/lab/Alert";
 import Checkbox from "@material-ui/core/Checkbox";
 import "./chat.css";
+import firebase from "./firebase";
+import "firebase/database";
 var Filter = require("bad-words"),
-	filter = new Filter();
+  filter = new Filter();
 
 function ChatBox() {
-	useEffect(() => {
-		filter.addWords("saala", "kutta", "harami");
-	}, []);
-	const [input, setInput] = useState("");
-	const [anonymous, setAnonymous] = useState(false);
-	const [showWarning, setShowWarning] = useState(false);
-	const [messages, setMessage] = useState([
-		{
-			userName: "abc@gmail.com",
-			userId: "123",
-			userMessage:
-				"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aeneanc commodo ligula eget dolor.",
-		},
-		{
-			userName: "abc@gmail.com",
-			userId: "123",
-			userMessage:
-				"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aeneanc commodo ligula eget dolor.",
-		},
-	]);
-	const sendDoubt = () => {
-		if (input.trim().length > 0 && filter.clean(input) === input) {
-			console.log(filter.clean(input) !== input);
-			console.log(input);
-			setInput("");
-		} else {
-			setShowWarning(true);
-			setTimeout(() => {
-				setShowWarning(false);
-			}, 2000);
-		}
-	};
+  const [input, setInput] = useState("");
+  const [anonymous, setAnonymous] = useState(false);
+  const [showWarning, setShowWarning] = useState(false);
+  const [messages, setMessage] = useState([
+    // {
+    //   userName: "abc@gmail.com",
+    //   userId: "123",
+    //   userMessage:
+    //     "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aeneanc commodo ligula eget dolor.",
+    // },
+    // {
+    //   userName: "abc@gmail.com",
+    //   userId: "123",
+    //   userMessage:
+    //     "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aeneanc commodo ligula eget dolor.",
+    // },
+  ]);
+  useEffect(() => {
+    filter.addWords("saala", "kutta", "harami");
 
-	return (
-		<div
-			id="chatBody"
-			style={{
-				boxShadow:
-					" 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
-			}}
-		>
-			<Snackbar
-				open={showWarning}
-				autoHideDuration={2000}
-				onClose={() => setShowWarning(false)}
-				style={{ marginBottom: 80 }}
-			>
-				<Alert variant="filled" severity="error">
-					Invalid Message
-				</Alert>
-			</Snackbar>
-			<main>
-				<header>
-					<div>
-						<h2>Classroom Chat</h2>
-					</div>
-				</header>
-				<ul id="chat">
-					{messages.map((item, index) => {
-						return (
-							<li
-								className={
-									item.userId === localStorage.getItem("userId") ? "me" : "you"
-								}
-							>
-								<div className="entete">
-									<h2>{item.userName}</h2>
-								</div>
-								<div className="triangle"></div>
-								<div className="message">{item.userMessage}</div>
-							</li>
-						);
-					})}
-					<li className="me">
-						<div className="entete">
-							<h2>Vincent</h2>
-						</div>
-						<div className="triangle"></div>
-						<div className="message">
-							Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-							commodo ligula eget dolor.
-						</div>
-					</li>
-				</ul>
-				<footer style={{ marginRight: 20 }}>
-					<TextField
-						variant="outlined"
-						multiline
-						value={input}
-						onChange={(e) => setInput(e.target.value)}
-						placeholder="Enter Doubt"
-						fullWidth
-						InputProps={{
-							endAdornment: (
-								<InputAdornment position="end">
-									<IconButton onClick={sendDoubt}>
-										<SendIcon />
-									</IconButton>
-								</InputAdornment>
-							),
-						}}
-					/>
-					<div align="right" style={{ fontSize: 12 }}>
-						<b>Disguise as Anonymous </b>
-						<Checkbox
-							style={{ marginTop: -5 }}
-							value={anonymous}
-							size="small"
-							color="primary"
-							onChange={() => setAnonymous(!anonymous)}
-						/>
-					</div>
-				</footer>
-			</main>
-		</div>
-	);
+    setMessage([]);
+    const onChildAdded = firebase
+      .database()
+      .ref(`Chats`)
+      .on("child_added", (snapshot) => {
+        let helperArr = [];
+        helperArr.push(snapshot.val());
+        setMessage((files) => [...files, ...helperArr]);
+        console.log(snapshot.val());
+      });
+    return () =>
+      firebase.database().ref("Chats").off("child_added", onChildAdded);
+  }, []);
+  const userName = localStorage.getItem("name");
+  const userId = localStorage.getItem("userId");
+  const sendDoubt = () => {
+    if (input.trim().length > 0 && filter.clean(input) === input) {
+      console.log(filter.clean(input) !== input);
+      console.log(input);
+      const uniqueKey = firebase.database().ref().push().key;
+      firebase.database().ref(`Chats/${uniqueKey}`).update({
+        userName: userName,
+        userId: userId,
+        userMessage: input,
+        anonymous: anonymous,
+      });
+      setInput("");
+    } else {
+      setShowWarning(true);
+      setTimeout(() => {
+        setShowWarning(false);
+      }, 2000);
+    }
+  };
+
+  return (
+    <div
+      id="chatBody"
+      style={{
+        boxShadow:
+          " 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+      }}
+    >
+      <Snackbar
+        open={showWarning}
+        autoHideDuration={2000}
+        onClose={() => setShowWarning(false)}
+        style={{ marginBottom: 80 }}
+      >
+        <Alert variant="filled" severity="error">
+          Invalid Message
+        </Alert>
+      </Snackbar>
+      <main>
+        <header>
+          <div>
+            <h2>Classroom Chat</h2>
+          </div>
+        </header>
+        <ul id="chat">
+          {messages.map((item, index) => {
+            return (
+              <li
+                className={
+                  item.userId === localStorage.getItem("userId") ? "me" : "you"
+                }
+              >
+                <div className="entete">
+                  <h2>{item.anonymous ? "Anonymous" : item.userName}</h2>
+                </div>
+                <div className="triangle"></div>
+                <div className="message">{item.userMessage}</div>
+              </li>
+            );
+          })}
+          {/* <li className="me">
+            <div className="entete">
+              <h2>Vincent</h2>
+            </div>
+            <div className="triangle"></div>
+            <div className="message">
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+              commodo ligula eget dolor.
+            </div>
+          </li> */}
+        </ul>
+        <footer style={{ marginRight: 20 }}>
+          <TextField
+            variant="outlined"
+            multiline
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="Enter Doubt"
+            fullWidth
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton onClick={sendDoubt}>
+                    <SendIcon />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+          <div align="right" style={{ fontSize: 12 }}>
+            <b>Disguise as Anonymous </b>
+            <Checkbox
+              style={{ marginTop: -5 }}
+              value={anonymous}
+              size="small"
+              color="primary"
+              onChange={() => setAnonymous(!anonymous)}
+            />
+          </div>
+        </footer>
+      </main>
+    </div>
+  );
 }
 
 export default ChatBox;
